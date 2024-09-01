9b20fe5488ce56e808f06da63601fa060a403a54
ahmedawadio <ahmed@ahmedawad.io>
2024-09-01 10:32:46 -0400

    pressable home

--- components/header.tsx
+++ components/header.tsx
@@ -52,12 +52,16 @@ async function UserOrLogin() {
 export function Header() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
-    <div className="flex items-center">
+  
+  
+    <a href="/" className="flex items-center cursor-pointer">
       <div style={{ margin: '0 10px' }}>
         <TbHexagonLetterA size={30} color="#A1A1AA" />
       </div>
       <span className="text-2xl font-bold">Apollo Clinic</span>
-    </div>
+    </a>
+
+
     <div className="hidden sm:flex items-center justify-end space-x-2">
       <a
         target="_blank"
