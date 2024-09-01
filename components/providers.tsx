9f36acbc0db69d95963072f5cc5aad96b025e0ad
ahmedawadio <ahmed@ahmedawad.io>
2024-09-01 11:08:23 -0400

    adding analytics

--- components/providers.tsx
+++ components/providers.tsx
@@ -8,6 +8,8 @@ import { TooltipProvider } from '@/components/ui/tooltip'
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
+import { Analytics } from '@vercel/analytics/react';
+
 
 
 
@@ -15,6 +17,7 @@ export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider {...props}>
       <Theme>
+        <Analytics/>
       <SidebarProvider>
         <TooltipProvider>{children}</TooltipProvider>
         <SpeedInsights/>
