eb0dfe9213588f5c79cd483ec4858ad75e52d508
ahmedawadio <ahmed@ahmedawad.io>
2024-09-26 11:23:42 -0400

    linking back to main domain

--- components/header.tsx
+++ components/header.tsx
@@ -54,7 +54,7 @@ export function Header() {
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
   
   
-    <a href="/" className="flex items-center cursor-pointer">
+    <a href="https://www.ahmedawad.io/" className="flex items-center cursor-pointer">
       <div style={{ margin: '0 10px' }}>
         <TbHexagonLetterA size={30} color="#A1A1AA" />
       </div>
